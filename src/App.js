f7425b6b9cc7b0d80a2f535b650d50a1de36b30c
Matheus <matheus.pitta12@gmail.com>
2021-12-22 13:33:36 -0300

    react-admin struct

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// in src/App.js
+import * as React from "react";
+import { Admin, Resource, EditGuesser } from 'react-admin';
+import { UserList } from './users'
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import { PostList, PostEdit, PostCreate } from './posts'
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+      <Admin authProvider={authProvider} dashboard={Dashboard} dataProvider={dataProvider}>
+          <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+          <Resource name="users" list={UserList} icon={UserIcon}/>
+      </Admin>
+  );
+
+export default App;
+
